578ba17e9dc0a36cf8a16c095c65bfe4935c3cd4
Ernesto Chero <ernesto.chero.contractor@bbva.com>
2020-03-27 16:26:02 -0500

    Adding some examples to SparkRecap

--- src/main/scala/part1recap/SparkRecap.scala
+++ src/main/scala/part1recap/SparkRecap.scala
@@ -1,19 +1,20 @@
 package part1recap
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
 
 object SparkRecap {
 
   // the entry point to the Spark structured API
-  val spark = SparkSession.builder()
+  val spark: SparkSession = SparkSession
+    .builder()
     .appName("Spark Recap")
     .master("local[2]")
     .getOrCreate()
 
   // read a DF
-  val cars = spark.read
+  val cars: DataFrame = spark.read
     .format("json")
     .option("inferSchema", "true")
     .load("src/main/resources/data/cars")
@@ -21,36 +22,39 @@ object SparkRecap {
   import spark.implicits._
 
   // select
-  val usefulCarsData = cars.select(
+  val usefulCarsData: DataFrame = cars.select(
     col("Name"), // column object
     $"Year", // another column object (needs spark implicits)
     (col("Weight_in_lbs") / 2.2).as("Weight_in_kg"),
     expr("Weight_in_lbs / 2.2").as("Weight_in_kg_2")
   )
 
-  val carsWeights = cars.selectExpr("Weight_in_lbs / 2.2")
+  val carsWeights: DataFrame = cars.selectExpr("Weight_in_lbs / 2.2")
 
   // filter
-  val europeanCars = cars.where(col("Origin") =!= "USA")
+  val europeanCars: Dataset[Row] = cars.where(col("Origin") =!= "USA")
 
   // aggregations
-  val averageHP = cars.select(avg(col("Horsepower")).as("average_hp")) // sum, meam, stddev, min, max
+  val averageHP: DataFrame = cars.select(
+    avg(col("Horsepower")).as("average_hp")
+  ) // sum, meam, stddev, min, max
 
   // grouping
-  val countByOrigin = cars
+  val countByOrigin: DataFrame = cars
     .groupBy(col("Origin")) // a RelationalGroupedDataset
     .count()
 
   // joining
-  val guitarPlayers = spark.read
+  val guitarPlayers: DataFrame = spark.read
     .option("inferSchema", "true")
     .json("src/main/resources/data/guitarPlayers")
 
-  val bands = spark.read
+  val bands: DataFrame = spark.read
     .option("inferSchema", "true")
     .json("src/main/resources/data/bands")
 
-  val guitaristsBands = guitarPlayers.join(bands, guitarPlayers.col("band") === bands.col("id"))
+  val guitaristsBands: DataFrame =
+    guitarPlayers.join(bands, guitarPlayers.col("band") === bands.col("id"))
   /*
     join types
     - inner: only the matching rows are kept
@@ -59,42 +63,64 @@ object SparkRecap {
    */
 
   // datasets = typed distributed collection of objects
-  case class GuitarPlayer(id: Long, name: String, guitars: Seq[Long], band: Long)
-  val guitarPlayersDS = guitarPlayers.as[GuitarPlayer] // needs spark.implicits
+  case class GuitarPlayer(id: Long,
+                          name: String,
+                          guitars: Seq[Long],
+                          band: Long)
+  val guitarPlayersDS
+    : Dataset[GuitarPlayer] = guitarPlayers.as[GuitarPlayer] // needs spark.implicits
   guitarPlayersDS.map(_.name)
 
   // Spark SQL
   cars.createOrReplaceTempView("cars")
-  val americanCars = spark.sql(
-    """
+  val americanCars = spark.sql("""
       |select Name from cars where Origin = 'USA'
-    """.stripMargin
-  )
+    """.stripMargin)
 
   // low-level API: RDDs
   val sc = spark.sparkContext
   val numbersRDD: RDD[Int] = sc.parallelize(1 to 1000000)
 
   // functional operators
-  val doubles = numbersRDD.map(_ * 2)
+  val doubles: RDD[Int] = numbersRDD.map(_ * 2)
 
   // RDD -> DF
-  val numbersDF = numbersRDD.toDF("number") // you lose type info, you get SQL capability
+  val numbersDF: DataFrame =
+    numbersRDD.toDF("number") // you lose type info, you get SQL capability
 
   // RDD -> DS
-  val numbersDS = spark.createDataset(numbersRDD)
+  val numbersDS: Dataset[Int] = spark.createDataset(numbersRDD)
 
   // DS -> RDD
-  val guitarPlayersRDD = guitarPlayersDS.rdd
+  val guitarPlayersRDD: RDD[GuitarPlayer] = guitarPlayersDS.rdd
 
   // DF -> RDD
-  val carsRDD = cars.rdd // RDD[Row]
+  val carsRDD: RDD[Row] = cars.rdd // RDD[Row]
 
   def main(args: Array[String]): Unit = {
     // showing a DF to the console
     cars.show()
     cars.printSchema()
 
+    // replace null by 0
+    val map_to_replace_null = Map("Miles_per_Gallon" -> 0)
+    cars.na.fill(map_to_replace_null).show()
 
+    // groupBy
+    val minAccelerationCondition = col("Acceleration") < 10
+    val groupByAcceleration = cars.groupBy(minAccelerationCondition).avg()
+    groupByAcceleration.show()
+    cars.filter(minAccelerationCondition).show()
+
+    // some operations
+    val weightInKgs = (col("Weight_in_lbs") * 0.45359237).as("Weight_in_Kgs")
+    val weightInLbs = col("Weight_in_lbs")
+    val carsInKgs: DataFrame =
+      cars.select(
+        weightInLbs,
+        weightInKgs,
+        (weightInKgs + weightInLbs).as("Total")
+      )
+    carsInKgs.show()
   }
 }
